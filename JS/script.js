53e8e8a96e8756e964c89796301a0d23e1624204
priyoucj <ucjpriyo@gmail.com>
2025-05-22 07:13:59 -0700

    Added project files with LF line endings

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+//used you responsive website (phoneview show)
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+ 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
